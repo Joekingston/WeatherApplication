e7cddecb9be87c8b1cc449d43dc1e189334e3453
Joseph <Jcarson3035@conestogac.on.ca>
2024-04-19 11:05:01 -0400

    added switch case

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -65,7 +65,67 @@ public class MainActivity extends AppCompatActivity{
         setBtn();
 
     }
+    private String setCondition(long code){
+        switch ((int) code) {
+            case 1000:
+                return "sunny";
+            case 1003:
+                return "cloudy";
+            case 1030:
+            case 1063:
+            case 1150:
+            case 1153:
+            case 1180:
+            case 1183:
+            case 1186:
+            case 1189:
+            case 1192:
+            case 1195:
+            case 1240:
+            case 1243:
+            case 1246:
+            case 1273:
+            case 1276:
+                return "rainy";
+            case 1087:
+                return "storm";
+            case 1006:
+            case 1009:
+            case 1114:
+            case 1117:
+            case 1135:
+            case 1147:
+                return "cloud";
+            case 1066:
+            case 1069:
+            case 1072:
+            case 1168:
+            case 1171:
+            case 1198:
+            case 1201:
+            case 1204:
+            case 1207:
+            case 1237:
+            case 1252:
+            case 1255:
+            case 1258:
+            case 1261:
+            case 1264:
+            case 1279:
+            case 1282:
+                return "rainy";
+            case 1210:
+            case 1213:
+            case 1216:
+            case 1219:
+            case 1222:
+            case 1225:
+                return "snowy";
+            default:
+                return "cloud";
+        }
 
+    }
     private void setBtn() {
         Button nextButton = findViewById(R.id.nextBTN);
 
@@ -108,7 +168,8 @@ public class MainActivity extends AppCompatActivity{
                 String itemHour = threeDayForecastPOJO.getForecast().getForecastday().get(0).getHour().get(i).getTime();
                 Double itemTemp = threeDayForecastPOJO.getForecast().getForecastday().get(0).getHour().get(i).getTempC();
                 //TODO get weather code
-                items.add(new Hourly(itemHour, itemTemp.intValue(), "cloudy"));
+                String weatherCode = setCondition(threeDayForecastPOJO.getForecast().getForecastday().get(0).getDay().getCondition().getCode());
+                items.add(new Hourly(itemHour, itemTemp.intValue(), weatherCode));
             }
 
         }
